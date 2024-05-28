2902c11f0be48d828d8081e5e357a664b0322309
Roialfassi <roialfassi@gmail.com>
2024-05-28 12:29:09 +0300

    Initial commit

---
+++ AVL.py
@@ -0,0 +1,244 @@
+import graphviz
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = 1
+
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.value
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.value
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.value
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.value
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+def print_tree(node, indent="", last='updown'):
+    """ A more visually appealing way to display the tree """
+    if node is not None:
+        print(indent, end='')
+        if last == 'updown':
+            print("Root----", end='')
+            indent += "       "
+        elif last == 'right':
+            print("R----", end='')
+            indent += "|      "
+        elif last == 'left':
+            print("L----", end='')
+            indent += "       "
+
+        print(str(node.value) + f" (H: {node.height})")
+
+        if node.left or node.right:
+            if node.left:
+                print_tree(node.left, indent, 'left')
+            if node.right:
+                print_tree(node.right, indent, 'right')
+    else:
+        print("Empty tree")
+
+
+# From       https://www.geeksforgeeks.org/avl-tree-in-python/
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def height(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def balance(self, node):
+        if not node:
+            return 0
+        return self.height(node.left) - self.height(node.right)
+
+    def insert(self, root, value):
+        if not root:
+            return Node(value)
+        elif value < root.value:
+            root.left = self.insert(root.left, value)
+        else:
+            root.right = self.insert(root.right, value)
+
+        root.height = 1 + max(self.height(root.left), self.height(root.right))
+        balance = self.balance(root)
+
+        # Left rotation
+        if balance > 1 and value < root.left.value:
+            return self.right_rotate(root)
+
+        # Right rotation
+        if balance < -1 and value > root.right.value:
+            return self.left_rotate(root)
+
+        # Left-Right rotation
+        if balance > 1 and value > root.left.value:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+
+        # Right-Left rotation
+        if balance < -1 and value < root.right.value:
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
+
+        return root
+
+    def delete(self, root, value):
+        if not root:
+            return root
+
+        if value < root.value:
+            root.left = self.delete(root.left, value)
+        elif value > root.value:
+            root.right = self.delete(root.right, value)
+        else:
+            if not root.left:
+                temp = root.right
+                root = None
+                return temp
+            elif not root.right:
+                temp = root.left
+                root = None
+                return temp
+
+            temp = self.min_value_node(root.right)
+            root.value = temp.value
+            root.right = self.delete(root.right, temp.value)
+
+        if not root:
+            return root
+
+        root.height = 1 + max(self.height(root.left), self.height(root.right))
+        balance = self.balance(root)
+
+        # Left rotation
+        if balance > 1 and self.balance(root.left) >= 0:
+            return self.right_rotate(root)
+
+        # Right rotation
+        if balance < -1 and self.balance(root.right) <= 0:
+            return self.left_rotate(root)
+
+        # Left-Right rotation
+        if balance > 1 and self.balance(root.left) < 0:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+
+        # Right-Left rotation
+        if balance < -1 and self.balance(root.right) > 0:
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
+
+        return root
+
+    def left_rotate(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self.height(z.left), self.height(z.right))
+        y.height = 1 + max(self.height(y.left), self.height(y.right))
+
+        return y
+
+    def right_rotate(self, z):
+        y = z.left
+        T3 = y.right
+
+        y.right = z
+        z.left = T3
+
+        z.height = 1 + max(self.height(z.left), self.height(z.right))
+        y.height = 1 + max(self.height(y.left), self.height(y.right))
+
+        return y
+
+    def min_value_node(self, root):
+        current = root
+        while current.left:
+            current = current.left
+        return current
+
+    def search(self, root, value):
+        if not root or root.value == value:
+            return root
+        if root.value < value:
+            return self.search(root.right, value)
+        return self.search(root.left, value)
+
+    def insert_value(self, value):
+        self.root = self.insert(self.root, value)
+
+    def delete_value(self, value):
+        self.root = self.delete(self.root, value)
+
+    def search_value(self, value):
+        return self.search(self.root, value)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    tree = AVLTree()
+
+    tree.insert_value(10)
+    tree.root.display()
+    tree.insert_value(20)
+    tree.root.display()
+    tree.insert_value(30)
+    tree.root.display()
+    tree.insert_value(40)
+    tree.root.display()
+    tree.insert_value(50)
+    tree.root.display()
+    tree.insert_value(60)
+    tree.root.display()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
